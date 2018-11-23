f5f2394457d66fcee42c285d3044cbdfa18b8aa7
Anwar <anwar.hemdene@gmail.com>
2018-11-23 11:56:07 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testFilter';
+  title = 'filter';
+  queryString ;
+
+  listofstudent = [
+    {id:"1", name:"icha",age:"20"},
+    {id:"2", name:"denok",age:"23"},
+    {id:"3", name:"sri utami",age:"10"},
+    {id:"4", name:"mbok darmi",age:"40"},
+    {id:"5", name:"jeniffer",age:"30"},
+    {id:"6", name:"limbuk",age:"22"}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterdataPipe } from './filterdata.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterdataPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
